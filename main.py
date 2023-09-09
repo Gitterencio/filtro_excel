9f96ffdc9d9e60a758cc9d839bc2890cda4fb720
Terencia Alberto Matamoros <tmatamoros@cognitiva.biz>
2023-09-09 09:05:51 -0600

    eliminar registro duplicado por completo

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
-from pathlib import Path
+from datetime import datetime
 
+#LOS INPUTS PUEDEN SER REEMPLAZADOS POR STRINGS DIRECTAMENTE
+#RUTA DEL ARCHIVO EXCEL
 path = input('ARCHIVO EXCEL\n')
 path = path.replace("'", '')
-sheet= input('HOJA\n')
-
-    
+#NOMBRE DE LA HOJA
+sheet= input('NOMBRE DE LA HOJA(OPCIONAL)\n')
+#NOMBRE DE LA COLUMNA
+#columna = input('NOMBRE DE LA COLUMNA A EVALUEAR\n')
+columna = 'COLUMNA B'
+#ELIMANR DUPLICADO O COPIAR UNA SOLA VEZ
+del_reg = int(input('0: Copiar si duplicados\n1: Eliminar duplicados\n'))
 
 if path != '' and sheet!= '':
     df = pd.read_excel(io = path, sheet_name=sheet)
@@ -22,12 +28,24 @@ elif path != '':
 
     
     for i in range(0,len(index)):
-        if not df.iloc[i]['COLUMNA A'] in saved:
-            saved.append(df.iloc[i]['COLUMNA A'])
+        if not df.iloc[i][columna] in saved:
+            saved.append(df.iloc[i][columna])
             for j in range(0,len(col)):
                 c = col[j]
                 data[c].append(df.iloc[i][c])
-               
+        
+        
+        elif del_reg:
+            try:
+                index = data[columna].index(df.iloc[i][columna])
+
+                for j in range(0,len(col)):
+                    c = col[j]
+                    del data[c][index]   
             
+            except ValueError:
+                print(ValueError)
+
+    
     new = pd.DataFrame(data)
-    new.to_excel('output.xlsx', index=False)
+    new.to_excel(f'{datetime.now()}_output.xlsx', index=False)
